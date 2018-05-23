8fffc33fd4b9f4d7bae33a004354c066e1a0c09e
Johnny Soong <johnnysoong@hotmail.com>
2018-05-23 18:39:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -33,11 +33,8 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  var array = kittens.slice(0, kittens.length);
-  array.pop();
-  return array;
 }
 
 function removeFirstKitten() {
-  
+  var array = kittens.slice(0, kittens.length);
 }
